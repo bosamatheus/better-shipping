6592156e3c7b0f4d0899785762953f8837ac78d3
Matheus Bosa <matheusbozza@gmail.com>
2022-03-10 22:40:07 -0300

    test(shipping-recommendations): :test_tube: No shipping options available test case

---
+++ shipping-recommendations/docs/notes.md
@@ -0,0 +1,14 @@
+# Notes
+
+## Design Patterns / Principles / Best Practices
+
+- Clean Architecture
+- Standard Go Project Layout (https://github.com/golang-standards/project-layout)
+- Single Responsibility Principle (SRP)
+- Test-Driven Development (TDD)
+
+## Anti-Patterns / Code Smells
+
+- High Coupling
+- God Class
+- Long Method
---
+++ shipping-recommendations/internal/usecase/recommendation/service_test.go
@@ -0,0 +1,33 @@
+package recommendation
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestService_GetRecommendations(t *testing.T) {
+	externalAPI := "/api/v1/shipping-options"
+	s := &Service{
+		repo: repository.NewShippingOptionsAPIClient(externalAPI),
+	}
+
+	testCases := []struct {
+		name    string
+		want    []vo.ShippingOption
+		wantErr error
+	}{
+		{
+			"No shipping options available", []vo.ShippingOption{}, nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := s.GetRecommendations()
+
+			assert.Equal(t, tc.want, got)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
