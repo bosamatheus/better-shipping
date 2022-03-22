006017d9fd34056b53a655b13825aa13e793e382
Matheus Bosa <matheusbozza@gmail.com>
2022-03-22 13:11:57 -0300

    feat(shipping-options): :sparkles: Create API server using Actix

--- shipping-options/README.md
+++ shipping-options/README.md
@@ -5,3 +5,4 @@ RESTful API that list different shipping options to order.
 ## Dependencies
 
 - Rust 1.59 (https://www.rust-lang.org)
+- Actix Web 4 (https://actix.rs)
--- shipping-options/src/main.rs
+++ shipping-options/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn shipping_options_api() -> impl Responder {
+    HttpResponse::Ok().body("Shipping Options API")
+}
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("OK")
+}
+
+#[get("/shipping-options")]
+async fn list_shipping_options() -> impl Responder {
+    HttpResponse::Ok().body("list shipping options endpoint")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(shipping_options_api)
+            .service(health)
+            .service(web::scope("/api/v1").service(list_shipping_options))
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
 }
